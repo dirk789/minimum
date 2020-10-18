814d9d67b977849cac3cf97d2c2ab11fec32f585
Dirk <dirkthomasmulder@gmail.com>
2020-10-18 14:12:40 +0700

    added swiperjs

--- minimum.php
+++ minimum.php
@@ -17,6 +17,10 @@ function theme_gsap_script() {
     wp_enqueue_script( 'gsap-scroll-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js', array(), false, true );
     wp_enqueue_script( 'gsap-js2', plugin_dir_url( __FILE__ ) . '/js/script.js', array(), false, true );
 
+    //swiperjs
+    wp_register_style( 'swipercss', 'https://unpkg.com/swiper/swiper-bundle.min.css' );
+    wp_enqueue_script( 'gsap-scroll-js', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), false, true );
+
     if (is_page( 'Homepage' )) {
         wp_enqueue_script('homepage-animation', plugin_dir_url( __FILE__ ) . '/js/homepage.js', array(), false, true);
     }
