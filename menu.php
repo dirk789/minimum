d79c2860cbe329ad68120b5d6a9876f89b26d5f1
Dirk <dirkthomasmulder@gmail.com>
2020-10-27 08:09:38 +0700

    changes for mega menu

--- menu.php
+++ menu.php
@@ -7,3 +7,22 @@ function wpmm_setup() {
 }
 add_action( 'after_setup_theme', 'wpmm_setup' );
 
+function wpmm_init() {
+    $location = 'mega_menu';
+    $css_class = 'has-mega-menu';
+    $locations = get_nav_menu_locations();
+    if ( isset( $locations[ $location ] ) ) {
+        $menu = get_term( $locations[ $location ], 'nav_menu' );
+        if ( $items = wp_get_nav_menu_items( $menu->name ) ) {
+            foreach ( $items as $item ) {
+                if ( in_array( $css_class, $item->classes ) ) {
+                    register_sidebar( array(
+                        'id'   => 'mega-menu-widget-area-' . $item->ID,
+                        'name' => $item->title . ' - Casanha Mega Menu',
+                    ) );
+                }
+            }
+        }
+    }
+}
+add_action( 'widgets_init', 'wpmm_init' );
