57e5b1afb6318b10eb2a3bab9681384197ca6103
Dirk <dirkthomasmulder@gmail.com>
2020-10-27 10:11:23 +0700

    added shopping cart

--- header.php
+++ header.php
@@ -1,5 +1,35 @@
 <?php 
 
+// CART Function
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
+/**
+ * Add AJAX Shortcode when cart contents update
+ */
+function woo_cart_but_count( $fragments ) {
+ 
+    ob_start();
+    
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+    
+    ?>
+    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'View your shopping cart' ); ?>">
+	<?php
+    if ( $cart_count > 0 ) {
+        ?>
+        <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php            
+    }
+        ?></a>
+    <?php
+ 
+    $fragments['a.cart-contents'] = ob_get_clean();
+     
+    return $fragments;
+}
+
+// MAIN MENU
+
 function casanhaheader_function() {
    
     echo "
